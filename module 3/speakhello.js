ef14141bebaef8104469f9814243b4dea69790dd
tanvimalik2000 <72303346+tanvimalik2000@users.noreply.github.com>
2020-11-01 17:06:46 +0530

    Update speakhello.js

--- module 3/speakhello.js
+++ module 3/speakhello.js
@@ -1,9 +1,9 @@
-(function(window) {
-	var speakWord = "Hello";
-	var helloSpeaker = function (name) {
-		console.log(speakWord + " " + name);
-	}
-
-	window.helloSpeaker = helloSpeaker;
-
-})(window);
+(function (window) {  
+    var helloSpeaker = {};
+    var speakWord = "Hello";
+    
+    helloSpeaker.speak = function (names) {
+      console.log(speakWord + " " + names);
+    }
+        window.helloSpeaker = helloSpeaker;
+    })(window);
