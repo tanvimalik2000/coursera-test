8fdb2881999a372a7882d050ea058768832a0e3c
tanvimalik2000 <72303346+tanvimalik2000@users.noreply.github.com>
2020-11-01 17:06:06 +0530

    Update speakgoodbye.js

--- module 3/speakgoodbye.js
+++ module 3/speakgoodbye.js
@@ -1,9 +1,10 @@
-(function(window) {
-	var speakWord = "Good Bye";
-	var byeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-	
-	window.byeSpeaker = byeSpeaker;
-
-})(window);
+(function (window){
+    var byeSpeaker = {};
+    var speakWord = "Good Bye";
+    
+    byeSpeaker.speak = function (names) {
+      console.log(speakWord + " " + names);
+    }
+     
+     window.byeSpeaker = byeSpeaker;
+    })(window);
